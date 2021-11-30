82a49747b13e0ed31d448be840991d940abc0775
Harun <harunalnayeem@gmail.com>
2021-12-01 02:59:01 +0600

    Designing Menuitems

--- src/Components/Body/MenuItem.js
+++ src/Components/Body/MenuItem.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Card,CardBody,CardImg,CardImgOverlay,CardTitle} from 'reactstrap'
 
 export default function MenuItem(props) {
-    console.log(props)
+    
     return (
         <div>
-            
+            <Card style={{margin :'10px'}}>
+                <CardBody>
+                    <CardImg width='100%' alt={props.dish.name} src={props.dish.image} style={{opacity :'0.6'}} />
+                    <CardImgOverlay>
+                        <CardTitle>
+                            {props.dish.name}
+                        </CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </Card>
         </div>
     )
 }
